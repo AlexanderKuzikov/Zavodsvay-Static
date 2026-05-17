b2fee2de6d9d7e9cc76caf437b0b10dcb77208e3
AlexanderKuzikov <alexander@kuzikov.com>
2026-05-17 20:16:55 +0500

    fix: article_published/modified 2024-01-01 -> 2026-01-01 for all 31 articles

--- pages/articles/bania/index.php
+++ pages/articles/bania/index.php
@@ -4,8 +4,8 @@ $meta_description = isset($meta_description) ? $meta_description : '';
 $canonical = "https://zavodsvay.ru/articles/bania/";
 $og_type = 'article';
 $schema_type = 'Article';
-$article_published = '2024-01-01'; // TODO: уточнить реальную дату
-$article_modified  = '2024-01-01'; // TODO: уточнить реальную дату
+$article_published = '2026-01-01';
+$article_modified  = '2026-01-01';
 
 ob_start();
 readfile(__DIR__ . '/content.html');
--- pages/articles/brevno/index.php
+++ pages/articles/brevno/index.php
@@ -1,11 +1,11 @@
 <?php
-$title = isset($title) ? $title : 'Сваи для бревнчатого дома — Гефест';
+$title = isset($title) ? $title : 'Сваи для бревенчатого дома — Гефест';
 $meta_description = isset($meta_description) ? $meta_description : '';
 $canonical = "https://zavodsvay.ru/articles/brevno/";
 $og_type = 'article';
 $schema_type = 'Article';
-$article_published = '2024-01-01'; // TODO: уточнить реальную дату
-$article_modified  = '2024-01-01'; // TODO: уточнить реальную дату
+$article_published = '2026-01-01';
+$article_modified  = '2026-01-01';
 
 ob_start();
 readfile(__DIR__ . '/content.html');
