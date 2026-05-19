22beec1215a6b008b452ec67fd81e57a9dbd86a7
AlexanderKuzikov <alexander@kuzikov.com>
2026-05-19 07:11:19 +0500

    fix(montage): подключить catalog.css через extra_css

--- pages/montage/index.php
+++ pages/montage/index.php
@@ -2,6 +2,7 @@
 $title = "Услуги монтажа винтовых свай — Гефест";
 $meta_description = "Профессиональный монтаж винтовых свай. Монтаж в день доставки. Гарантия.";
 $canonical = "https://zavodsvay.ru/montage/";
+$extra_css = '<link rel="stylesheet" href="/assets/css/catalog.css">';
 
 ob_start();
 readfile(__DIR__ . '/content.html');
